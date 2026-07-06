cb9db8aadca3e3a133be1663b947de162b675faa
derivgoo <boidetergent@gmail.com>
2026-07-06 23:28:11 +0700

    Add files via upload

---
+++ one/uae_files/uae.UserScripts.ConsoleOverride__CRHWwmRhL7zNwsf7WsSYwQ__66jB.js
@@ -0,0 +1,8 @@
+﻿const hostname = window.location.hostname;
+const isProductionHub = ["app.deriv.ae"].includes(hostname);
+
+if (isProductionHub) {
+    if (window.console?.log) {
+        window.console.log = function () {};
+    }
+}
