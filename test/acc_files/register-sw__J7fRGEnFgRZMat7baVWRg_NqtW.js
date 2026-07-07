932e2fa43149de8c474218fd64690e1599df565a
derivgoo <boidetergent@gmail.com>
2026-07-07 19:07:16 +0700

    Add files via upload

---
+++ test/acc_files/register-sw__J7fRGEnFgRZMat7baVWRg_NqtW.js
@@ -0,0 +1,1 @@
+﻿if (typeof navigator !== "undefined" && "serviceWorker" in navigator && !(/OutSystemsApp/i).test(navigator.userAgent)) { navigator.serviceWorker.register("/uae/sw.js"); }
---
+++ test/acc_files/uae.UserScripts.CancellablePromise__ZssqXCMCgPexyfYPIjGt_NqtW.js
@@ -0,0 +1,22 @@
+﻿function createCancellablePromise(promiseFactory) {
+  let isCancelled = false;
+  
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promiseFactory
+      .then(value => {
+        if (!isCancelled) resolve(value);
+      })
+      .catch(error => {
+        if (!isCancelled) reject(error);
+      });
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel: () => {
+      isCancelled = true;
+    }
+  };
+}
+
+window.createCancellablePromise = createCancellablePromise;
